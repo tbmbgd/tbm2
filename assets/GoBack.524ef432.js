a4d7a3224dc117f1920d60160ac4257a9fd44acc
tbmbgd <g.sablico@gmail.com>
2023-04-22 16:23:44 +0200

    Corrections de certaines Vignettes

--- assets/GoBack.524ef432.js
+++
@@ -1,1 +0,0 @@
-import{o,e as s,i as t,j as a}from"./index.addd6d8a.js";import{_ as c}from"./plugin-vue_export-helper.21dcd24c.js";const n={},l={class:"q-mt-xl",align:"center"};function i(r,e){return o(),s("div",l,[t(a,{onClick:e[0]||(e[0]=m=>r.$router.back()),push:"",class:"glossy text-blue-grey-12",color:"primary",label:"RETOUR"})])}var f=c(n,[["render",i]]);export{f as G};
--- assets/use-dark.7791588b.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./index.a84700d4.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
